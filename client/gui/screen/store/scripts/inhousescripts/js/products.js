9f9f3446a71d4407a30fcbd09482ed1634b97b47
kaustav-inbetween <kaustav@inbetween.co.in>
2014-11-16 14:36:11 +0530

    products jsonp fetch issue resolved

--- client/gui/screen/store/scripts/inhousescripts/js/products.js
+++ client/gui/screen/store/scripts/inhousescripts/js/products.js
@@ -3,13 +3,14 @@ function createProductThumbs (products) {
     var $container = $('#product-container');
     var thumbs = '';
 
+    $container.mixItUp('destroy', true);
     $container.empty();
 
     for (var productCount = 0; productCount < products.length; productCount++) {
         var product = products[productCount];
 
         thumbs += '<div class="mix product-thumb">' +
-            '<img class="thumb-img" src="../img/categories/' + product.img + '.png" />' +
+            '<img class="thumb-img" src="../img/products/' + product.mainImg + '.png" />' +
             '</div>';
     }
 
